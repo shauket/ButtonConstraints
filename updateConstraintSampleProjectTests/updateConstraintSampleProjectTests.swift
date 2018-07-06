77c8a89d953761148fc1d8a469705d5881a27c75
muhammad shauket <muhammad.shauket@rakuten.com>
2018-07-06 11:11:08 +0800

    Initial Commit

---
+++ updateConstraintSampleProjectTests/updateConstraintSampleProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  updateConstraintSampleProjectTests.swift
+//  updateConstraintSampleProjectTests
+//
+//  Created by Shauket, Muhammad on 2018/07/06.
+//  Copyright © 2018 Rakuten. All rights reserved.
+//
+
+import XCTest
+@testable import updateConstraintSampleProject
+
+class updateConstraintSampleProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
